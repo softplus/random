14e9edc9db1a8a340481b4b7de54f2c4da364efa
John Mueller <softplus@users.noreply.github.com>
2020-10-16 22:30:38 +0200

    Update touch_keyboard.ino

--- SimpleTouchKeyboard/touch_keyboard.ino
+++ SimpleTouchKeyboard/touch_keyboard.ino
@@ -11,19 +11,17 @@
 // Uses libraries:
 // Keyboard
 //   generic Arduino USB keyboard library
+//   https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
 // ADCTouch
 //   capacative touch sensor library
-//   from https://github.com/martin2250/ADCTouch
+//   https://github.com/martin2250/ADCTouch
 //   Available in Arduino libraries
 //
 
-// What it does:
-// Waits 
-
-// two LED pins on Pro Micro are 17 & 30; we'll just 17
+// two LED pins on Pro Micro are 17 & 30; we'll use 17
 #define LED_PIN 17
 
-// where we're reading the touch
+// where we're reading the touch, no particular reason why I chose this one
 #define PIN_TOUCH A3
 
 #include <Keyboard.h>
@@ -40,18 +38,18 @@ void setup() {
   referenceValue = ADCTouch.read(PIN_TOUCH, 500);
 }
 
-uint32_t blinkUntil = 0;    // how long to blink
-uint32_t blinkNext = 0;     // when to blink on/off next
-int blinkState = 0;         // current blinking state (0=off)
-uint32_t debounceWait = 0;  // to catch & ignore flakey presses
+uint32_t blinkUntil = 0;      // how long to blink
+uint32_t blinkNext = 0;       // when to blink on/off next
+int blinkState = 0;           // current blinking state (0=off)
+uint32_t debounceWait = 0;    // to catch & ignore flakey presses
 
 // the loop function runs over and over again, forever
 
 void loop() {
   // read touch pin and wait for a sign
   int pinValue = ADCTouch.read(PIN_TOUCH) - referenceValue;   // get touch value
-  if (pinValue > 100) {     // yay, someone touched it!
-    if (millis() > debounceWait) {      // sometimes the touch is flakey, just trigger once every 300ms
+  if (pinValue > 100) {                      // yay, someone touched it!
+    if (millis() > debounceWait) {           // sometimes the touch is flakey, just trigger once every 300ms
       debounceWait = millis() + 300;
       // Serial.println("Touch registered"); // sent to serial port for debugging, not really needed
 
@@ -76,7 +74,7 @@ void loop() {
         blinkNext = millis() + 100;    // blink in 200ms interval
       }      
     } else {                           // can we stop blinking now?
-      digitalWrite(LED_PIN, HIGH );   // turn the LED off
+      digitalWrite(LED_PIN, HIGH );    // turn the LED off
       blinkUntil = 0;      
     }
   }
